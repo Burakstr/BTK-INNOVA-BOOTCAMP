47f5e885bd50e63de47414032e91c109ad4940f0
Burak <145280474+Burakstr@users.noreply.github.com>
2024-10-01 11:32:56 +0300

    Add files via upload

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,144 @@
+import UIKit
+
+//Burak Satır
+
+var ilce = "Kağıthane"
+print("İlçe                  : \(ilce)")
+
+var kita = "Avrupa"
+print("Kıta                  : \(kita)")
+
+var faks = "0212"
+print("Faks                  : \(faks)")
+
+var postaKodu = 34000
+print("Posta Kodu            : \(postaKodu)")
+
+var instaAdresi = "Burakstrr"
+print("İnstagram adresi      : \(instaAdresi)")
+
+var calistiginBolum = "iOS Developer"
+print("Çalıştığın Bölüm      : \(calistiginBolum)")
+
+var urunMiktari = 381
+print("Ürün Miktarı          : \(urunMiktari)")
+
+var musteriSoyadi = "Satır"
+print("Müşteri Soyadı        : \(musteriSoyadi)")
+
+var odemeMiktari = 5555.55
+print("Ödeme Miktarı         : \(odemeMiktari)")
+
+var dogumTarihi = "19.10.1999"
+print("Doğum Tarihi          : \(dogumTarihi)")
+
+var borc = 6000.0
+print("Borç                  : \(borc)")
+
+var medeniHal = "Bekar"
+print("Medeni Hal            : \(medeniHal)")
+
+var videoYorumu = "Yararlı bir video olmuş."
+print("Video Yorumu          : \(videoYorumu)")
+
+var odemeSaati = "14:30"
+print("Ödeme Saati           : \(odemeSaati)")
+
+var eftMiktari = "10.000 ₺"
+print("EFT Miktari           : \(eftMiktari)")
+
+var satılanMiktar = 300
+print("Satılan Miktar        : \(satılanMiktar)")
+
+var telefonModeli = "iPhone 15 Pro"
+print("Telefon Modeli        : \(telefonModeli)")
+
+let dergiAdi = "Str Dergi"
+print("Dergi Adi             : \(dergiAdi)")
+
+let basimTarihi = "01.01.2001"
+print("Basım Tarihi          : \(basimTarihi)")
+
+let zamMiktari = 1000.00
+print("Zam Miktarı           : \(zamMiktari)")
+
+let daireSayisi = 134
+print("Daire Sayısı          : \(daireSayisi)")
+
+let enlem = 41.0717
+print("Enlem                 : \(enlem)")
+
+let boylam = 28.9646
+print("Boylam                : \(boylam)")
+
+let yemekAdi = "Pilav"
+print("Yemek Adı             : \(yemekAdi)")
+
+let urunFiyati = 100
+print("Ürün Fiyatı           : \(urunFiyati)")
+
+let sirket = "INNOVA"
+print("Şirket                : \(sirket)")
+
+let videoAdi = "iOS Eğitimi"
+print("Video Adı             : \(videoAdi)")
+
+let muzikSuresi = "3.24 Dakika"
+print("Müzik Süresi          : \(muzikSuresi)")
+
+let mekanPuani = "10/8"
+print("Mekan Puanı           : \(mekanPuani) Puan")
+
+let dosyaAdi = "Ödev1"
+print("Dosya Adı             : \(dosyaAdi)")
+
+let resimFormati = "PNG"
+print("Resim Formatı         : \(resimFormati)")
+
+let renk = "Siyah"
+print("Renk                  : \(renk)")
+
+let renkKodu = "#000000"
+print("Renk Kodu             : \(renkKodu)")
+
+let bilgisayarModeli = "MacBook Air"
+print("Bilgisayar Modeli     : \(bilgisayarModeli)")
+
+let ekranBoyutu = "13 inç"
+print("Ekran Boyutu          : \(ekranBoyutu)")
+
+let kullanımSuresi = "18 Saat"
+print("Kullanım Süresi       : \(kullanımSuresi)")
+
+let basınc = 32.0
+print("Basınç                : \(basınc)")
+
+let etkinlikGunu = "Cumartesi"
+print("Etkinlik Günü         : \(etkinlikGunu)")
+
+let odemeGunu = "20.10.2024"
+print("Ödeme Günü            : \(odemeGunu)")
+
+let yolculukCıkısTarihi = "14.09.2024"
+print("Yolculuk Çıkış Tarihi : \(yolculukCıkısTarihi)")
+
+let mahalleAdi = "Çeliktepe"
+print("Mahalle Adı           : \(mahalleAdi)")
+
+let otobusHatti = "500T"
+print("Otobüs Hattı          : \(otobusHatti)")
+
+let kullanilanDakika = 120
+print("Kullanılan Dakika     : \(kullanilanDakika)")
+
+let kargoTakipNo = "123456789QWERTY"
+print("Kargo Takip No        : \(kargoTakipNo)")
+
+let kuponSuresi = "30 Gün"
+print("Kupon Süresi          : \(kuponSuresi)")
+
+let kuponKodu = "iOS2024"
+print("Kupon Kodu            : \(kuponKodu)")
+
+let faturaTarihi = "20.10.1999"
+print("Fatura Tarihi         : \(faturaTarihi)")
